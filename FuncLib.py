8d922096e92f6d4c6185001ce41c4e7e72bf54c1
CN-Tower <247114045@qq.com>
2018-02-02 19:35:15 +0800

    Update FuncLib.py

--- FuncLib.py
+++ FuncLib.py
@@ -8,18 +8,12 @@
                                    Date: 2018-2-2
                                  GitHub: http://github.com/CN-Tower/dataTools
     -----------------------------------------------------------------------------------
-                                      * T.find
-                                      * T.find_index
-                                      * T.find_where
-                                      * T.contains
-                                      * T.reject
-                                      * T.every
-                                      * T.some
-                                      * T.uniq
-                                      * T.pluck
-                                      * T.list
-                                      * T.log
-                                      * T.now
+                          * T.find                 * T.find_index
+                          * T.find_where           * T.contains
+                          * T.reject               * T.every
+                          * T.some                 * T.uniq
+                          * T.pluck                * T.list
+                          * T.log                  * T.now
     ===================================================================================
 """
 import time
