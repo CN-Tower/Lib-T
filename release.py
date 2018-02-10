cfce6d5de3ba8c933a275d877be67e49c55bd434
CN-Tower <247114045@qq.com>
2018-02-10 14:59:37 +0800

    Update release.py

--- release.py
+++ release.py
@@ -7,6 +7,7 @@ import time
 def do_release():
     T.clear()
     tmp_files = ['build', 'dist', 'funclib.egg-info']
+    
     T.log('Deleting temporary files...')
     if platform.system() == "Windows":
         for file in tmp_files:
@@ -19,13 +20,30 @@ def do_release():
         del_files = T.each(lambda x: pwd_path + '/' + x, tmp_files)
         for file in del_files:
             os.system('rm -rf ' + file)
-    time.sleep(1)   
+    time.sleep(1)
+    
+    T.log('Rename README.md to README.rst...')
+    if platform.system() == "Windows":
+        os.system('ren README.md README.rst')
+    else:
+        os.system('mv README.md README.rst')
+    time.sleep(1)
+    
     T.log('Building Dist...')
     os.system('python setup.py sdist build')
     time.sleep(1)
+    
     T.log('Release...')
     os.system('twine upload dist/*')
     time.sleep(1)
+    
+    T.log('Rename README.rst to README.md...')
+    if platform.system() == "Windows":
+        os.system('ren README.rst README.md')
+    else:
+        os.system('mv README.rst README.md')
+    time.sleep(1)
+    
     T.log('Release Success!')
     
 if __name__ == '__main__':
