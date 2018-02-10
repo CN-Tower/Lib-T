d25d6539d99f741a1db8d98673b59c0b29e4bb1a
CN-Tower <247114045@qq.com>
2018-02-10 15:17:51 +0800

    Update release.py

--- release.py
+++ release.py
@@ -8,7 +8,9 @@ def do_release():
     T.clear()
     tmp_files = ['build', 'dist', 'funclib.egg-info']
     
-    T.log('Deleting temporary files...')
+    T.log('Release Starting ...')
+    
+    print('Deleting temporary files...')
     if platform.system() == "Windows":
         for file in tmp_files:
             if os.path.exists(file):
@@ -20,30 +22,35 @@ def do_release():
         for file in tmp_files:
             os.system('rm -rf ' + file)
     time.sleep(1)
+    print('Delete temporary files Success!')
     
-    T.log('Rename README.md to README.rst...')
+    print('Rename README.md to README.rst...')
     if platform.system() == "Windows":
         os.system('ren README.md README.rst')
     else:
         os.system('mv README.md README.rst')
     time.sleep(1)
+    print('Rename README.md to README.rst Success!')
     
-    T.log('Building Dist...')
+    print('Building Dist...')
     os.system('python setup.py sdist build')
     time.sleep(1)
+    print('Build Dist Success!')
     
-    T.log('Release...')
+    print('Release...')
     os.system('twine upload dist/*')
     time.sleep(1)
+    print('Release Success!')
     
-    T.log('Rename README.rst to README.md...')
+    print('Rename README.rst to README.md...')
     if platform.system() == "Windows":
         os.system('ren README.rst README.md')
     else:
         os.system('mv README.rst README.md')
     time.sleep(1)
+    print('Rename README.rst to README.md Success!')
     
-    T.log('Release Success!')
+    T.log('Congratulations, Release totaly Success!')
     
 if __name__ == '__main__':
     do_release()
