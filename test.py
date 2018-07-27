39f75828d3fde3627d837962b005a991c8041ec6
CN-Tower <cntower@yahoo.com>
2018-07-27 12:42:02 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 
 def test_funclib():
-    os.system('coverage run test/funclib.test.py')
+    os.system('coverage run funclib.test.py')
     os.system('coverage report -m')
 
 if __name__=='__main__':
